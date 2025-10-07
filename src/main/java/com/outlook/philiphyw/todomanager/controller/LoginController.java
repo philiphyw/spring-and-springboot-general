76ea23ce01177b342d75dd1664810dfc21ffcfdf
Philip HYW <75644553+philiphyw@users.noreply.github.com>
2025-10-07 13:23:56 -0400

    added logic to support optional param variable in login controller

--- src/main/java/com/outlook/philiphyw/todomanager/controller/LoginController.java
+++ src/main/java/com/outlook/philiphyw/todomanager/controller/LoginController.java
@@ -1,13 +1,18 @@
 package com.outlook.philiphyw.todomanager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class LoginController {
     @GetMapping("login")
-    public String showLoginJsp(){
+    public String showLoginJsp(@RequestParam(value="name",required = false) String name, ModelMap modelMap){
+        if(name != null){
+            modelMap.put("clientName",name);
+        }
         return "login";
     }
 }
