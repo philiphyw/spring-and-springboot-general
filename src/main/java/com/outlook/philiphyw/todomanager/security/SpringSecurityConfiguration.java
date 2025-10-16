52198b5851375a24d542957c64794734f81c08e0
Philip HYW <75644553+philiphyw@users.noreply.github.com>
2025-10-16 14:26:53 -0400

    customized SpringSecurity.SecurityFilterChain to enable H2 console access in local

--- src/main/java/com/outlook/philiphyw/todomanager/security/SpringSecurityConfiguration.java
+++ src/main/java/com/outlook/philiphyw/todomanager/security/SpringSecurityConfiguration.java
@@ -3,11 +3,16 @@ package com.outlook.philiphyw.todomanager.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -34,5 +39,17 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+//    Customize the default SecurityFilterChain to enable H2 console access in local
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable) // Lambda DSL for CSRF
+                .headers(headers -> headers    // Lambda DSL for headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                ).build();
     }
 }
